c24b262e969551f719abec8c4c75961b2fbd47a7
Pierre-Guillaume <pierreguillaumelaurin@gmail.com>
2016-05-19 11:39:04 -0400

    Change api from openweather to forecast.io

--- js/weatherapp.js
+++ js/weatherapp.js
@@ -4,15 +4,17 @@ $(document).ready(function() {
       var myLatitude = parseFloat(Math.round(position.coords.latitude * 100) / 100).toFixed(2);
       var myLongitude = parseFloat(Math.round(position.coords.longitude * 100) / 100).toFixed(2);
       
-  $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat="+myLatitude+"&lon="+myLongitude+"&id=524901&appid=ca8c2c7970a09dc296d9b3cfc4d06940", function(json) {
-    $('#location').html(json.name + ', ' + json.sys.country);
-    $('#temperature').html(parseInt(json.main.temp) - 275);
-    $('#cloud-forecast').html(json.weather[0].main + '/ '+ json.weather[0].description);
-    $('#unit').html('C');
+  $.ajax({
+      url: "https://api.forecast.io/forecast/df76430db2bad3f9ef6d03aebba0f0aa/"+myLatitude+","+myLongitude,
+      dataType: "jsonp",
+      success: function(json) {
     //WEATHER CONDITIONS FOUND HERE: http://openweathermap.org/weather-conditions
-    console.log(json.weather[0].main);
-    switch (json.weather[0].main) {
-      case "Clouds":
+    console.log(json);
+    var currentWeather = json.currently.icon;
+    switch (currentWeather) {
+      case "partly-cloudy-day":
+      case "partly-cloudy-night":
+      case "cloudy":
         $('#diagnostic-temperature').html("C'est gris dehors");
         $('#icon').addClass('icon cloudy');
         $('#icon').append('<div class="cloud"></div>');
@@ -20,7 +22,8 @@ $(document).ready(function() {
         $('#solution-temperature').html('Prends un café et ');
         console.log('ok');
         break;
-      case "Clear":
+      case "clear-day":
+      case "clear-night":
         $('#diagnostic-temperature').html("Il fait beau dehors");
         $('#icon').addClass('icon sunny clear');
         $('#icon').append('<div class="sun"></div>');
@@ -28,7 +31,8 @@ $(document).ready(function() {
         $('#solution-temperature').html('Prends une bière et ');
         console.log('non');
       break;
-      case "Snow":
+      case "snow":
+      case "sleet":
         $('#diagnostic-temperature').html("Il neige dehors???!!!!");
         $('#icon').addClass('icon ');
         $('#icon').append('<div class="cloud"></div>');
@@ -37,26 +41,34 @@ $(document).ready(function() {
         $('.snow').append('<div class="flake"></div>');
         $('#solution-temperature').html('Reste calme et ');
       break;
-      case "Drizzle":
-      case "Rain":
+      case "fog":
+      case "rain":
         $('#diagnostic-temperature').html("Il pleut dehors");
         $('#icon').addClass('icon rainy');
         $('#icon').append('<div class="cloud"></div>');
         $('#icon').append('<div class="rain"></div>');
         $('#solution-temperature').html('Prends un café et ');
         break;
-      case "Thunderstorm":
-        $('#diagnostic-temperature').html("C'est VRAIMENT pas beau dehors");
+      case "wind":
+        $('#diagnostic-temperature').html("Le temps est bizarre en ce moment");
         $('#icon').addClass('icon thunder-storm thunderstorm');
         $('#icon').append('<div class="cloud"></div>');
         $('#icon').append('<div class="lightning"></div>');
         $('.lightning').append('<div class="bolt"></div>');
         $('.lightning').append('<div class="bolt"></div>');
         $('#solution-temperature').html('Sors dehors ou ');
-    }
-  });        
-  });
-  }else{
+        break;
+      default:
+        $('#diagnostic-temperature').html("Le temps est indescriptible en ce moment");
+        $('#solution-temperature').html('Oublie le temps et ');
+        break;
+          }
+        }
+      });
+    });      
+    } else {
     $("#location").html("Ouvre ton géolocaliseur et")
-  }
+    }
+
 });
+
